9e809e8aa2deca82f041d1cc8c3522ee5624e03c
notPhani <phaniexplains14@gmail.com>
2025-11-08 18:39:10 +0530

    initialize empty gate, main, and qubit modules

---
+++ src/qubits.py
@@ -0,0 +1,8 @@
+"""
+This module defines the Qubit class, which represents a quantum bit in a quantum computing system.
+Here qubits will be implemented with basic properties and methods to manipulate their state.
+And will be written as tensors for compatibility with quantum Machine Learning frameworks.
+"""
+
+class Qubit:
+    def __init__(self, )
